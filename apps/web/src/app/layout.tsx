2f68b935fd84ede452e4c5d9cac6557246207964
Paskal Adams <paskaladams@gmail.com>
2024-02-26 20:23:07 +0700

    feat: setup package dan add model database

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <ChakraProvider>
+
+          {children}
+
+        </ChakraProvider>
       </body>
     </html>
   );
