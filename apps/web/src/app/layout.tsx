99802c165ae10753ebeb8024b320cd98482b4d50
bonadoni95 <148220399+bonadoni95@users.noreply.github.com>
2024-02-26 20:55:14 +0700

    Revert "feat: setup package dan add model database"

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,11 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ChakraProvider>
-
-          {children}
-
-        </ChakraProvider>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
