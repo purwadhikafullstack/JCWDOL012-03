769b366a1f61ece92c769b53b1f7750623a75bbb
ardvidn <ardvidn93@gmail.com>
2024-03-22 23:06:25 +0700

    feat: fixing after revert commit

--- apps/api/src/controllers/adminAuthorization/get.Store.Admin.By.Id.ts
+++ apps/api/src/controllers/adminAuthorization/get.Store.Admin.By.Id.ts
@@ -12,15 +12,15 @@ const GetStoreAdminById = async (req: Request, res: Response) => {
     const { id } = req.params;
     //cek role dari cookies
     //validasi superadmin
-    const getCookies = req.cookies['user-token'];
-    const cookiesToDecode = jwtDecode<jwtPayload>(getCookies);
+    // const getCookies = req.cookies['user-token'];
+    // const cookiesToDecode = jwtDecode<jwtPayload>(getCookies);
 
-    if (!cookiesToDecode) {
-      return res.status(401).json({
-        code: 401,
-        message: "you're not authorized",
-      });
-    }
+    // if (!cookiesToDecode) {
+    //   return res.status(401).json({
+    //     code: 401,
+    //     message: "you're not authorized",
+    //   });
+    // }
 
     const getDataById = await prisma.user.findUnique({
       where: {
