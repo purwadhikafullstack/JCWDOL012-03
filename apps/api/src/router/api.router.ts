5823d48d7f392d1e3491635842f9f1ada5b2474a
paskaladams <140481898+paskaladams@users.noreply.github.com>
2024-03-22 22:18:23 +0700

    Store management (#13)
    * feat: update header hero productlist footer
    
    * feat: user registration in progress 1
    
    * feat: update tampilan awal homepage
    
    * feat: update tampilan signin dan signup
    
    * feat: update fitur registrasi dan login
    
    * feat: update fitur registrasi dan login frontend
    
    * feat: setup google auth
    
    * feat: update backend verifikasi akun
    
    * feat: update ui signin signup
    
    * feat: update package.json
    
    * feat: update verifikasi done
    
    * feat: update profile page
    
    * feat: progress update profile
    
    * feat: update email dan avatar
    
    * feat: update fitur ganti password dan reset password
    
    * feat: update fitur create address
    
    * feat: update fitur edit delete dan set default
    
    * feat: comment sesuai update
    
    * fix: resolve merge conflict by incorporating both suggestions
    
    * fix: resolve merge conflict by incorporating both suggestions
    
    * fix: error fixed
    
    * feat: add missing code

--- apps/api/src/router/api.router.ts
+++ apps/api/src/router/api.router.ts
@@ -1,23 +1,16 @@
 import { Router } from 'express';
 import authRouter from '@/router/auth.router';
 import registerRouter from './register.router';
-<<<<<<< HEAD
 import profileRouter from './profile.router';
 import adminRouter from './admin.router';
 // import userRouter from '@/router/user.router';
-=======
-import productRouter from '@/routers/Product.And.Category.Management.Router';
-// import userRouter from '@/router/user.router';
-// import transactionRouter from '@/router/transaction.router';
-// import analyticsRouter from '@/router/analytics.router';
->>>>>>> adb62195e206f27bd7b7bd45023f2bbd77c803f1
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/register', registerRouter);
-apiRouter.use('/product', productRouter);
-
+apiRouter.use('/profile', profileRouter);
+apiRouter.use('/admin', adminRouter);
 // apiRouter.use('/user', userRouter);
 // apiRouter.use('/transaction', transactionRouter);
 // apiRouter.use('/analytics', analyticsRouter);
