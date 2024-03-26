ad1244d4fe5c2df9222e1a04bd638bf38778eb3d
ardvidn <ardvidn93@gmail.com>
2024-03-26 17:59:32 +0700

    feat: create API for search query

--- apps/api/src/router/api.router.ts
+++ apps/api/src/router/api.router.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import authRouter from '@/router/auth.router';
 import registerRouter from './register.router';
-import profileRouter from './profile.router';
-import adminRouter from './admin.router';
+// import profileRouter from './profile.router';
+// import adminRouter from './admin.router';
 // import userRouter from '@/router/user.router';
 import productRouter from '@/routers/Product.And.Category.Management.Router';
 import stockRouter from '@/routers/Stock.Management.Router';
@@ -10,8 +10,8 @@ import stockRouter from '@/routers/Stock.Management.Router';
 // import userRouter from '@/router/user.router';
 // import transactionRouter from '@/router/transaction.router';
 // import analyticsRouter from '@/router/analytics.router';
-import profileRouter from './profile.router';
-import adminRouter from './admin.router';
+
+import productCatalogAndSearchProductRouter from '@/routers/productCatalogAndProductSearch.router';
 // import userRouter from '@/router/user.router';
 
 const apiRouter = Router();
@@ -20,8 +20,9 @@ apiRouter.use('/auth', authRouter);
 apiRouter.use('/register', registerRouter);
 apiRouter.use('/product', productRouter);
 apiRouter.use('/stock', stockRouter);
-apiRouter.use('/profile', profileRouter);
-apiRouter.use('/admin', adminRouter);
+// apiRouter.use('/profile', profileRouter);
+// apiRouter.use('/admin', adminRouter);
+apiRouter.use('/productSearch', productCatalogAndSearchProductRouter);
 // apiRouter.use('/user', userRouter);
 // apiRouter.use('/transaction', transactionRouter);
 // apiRouter.use('/analytics', analyticsRouter);
